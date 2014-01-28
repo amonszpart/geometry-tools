b14c1cfd5cc4319a9f773ec34e74833a2a717121
Aron <aron.monszpart@gmail.com>
2014-01-28 15:46:23 +0000

    extractPlane documentation completed

--- PCLTools/include/Primitives.h
+++ PCLTools/include/Primitives.h
@@ -19,6 +19,20 @@ namespace smartgeom
         public:
             /// STATICs ///
 
+            /**
+             * @brief extractPlane Gets a ransac plane from a pointcloud
+             *
+             * @param[out] out_coefficients    A boost pointer to 4 coeffs: { Nx, Ny, Nz, d }, where N is the plane normal, and d is a distance from O
+             * @param[in]  in_cloud            Input pointcloud of points typed MyPointT
+             * @param[out] out_inlier_indices  Output inlier indices. Set to non NULL to request the output
+             * @param[in]  in_indices          Input indices indexing "in_cloud"
+             * @param[in]  distanceThreshold   Threshold to calcaulate inliers
+             * @param[in]  sacmodel            NORMAL_PLANE estimates a plane using normals. NORMAL_PARALLEL_PLANE estimates using normals, and tries to be parallel to "*p_normal"
+             * @param[in]  p_normal            A vector to be parallel to, NULL if unused.
+             * @param[in]  normal_estimation_K Normals get estimated using this K-nearest neighbours, <20 makes sense.
+             * @param[in]  sac_method          RANSAC, PROSAC, etc.
+             * @param[in]  seg_max_iterations  RANSAC iteration cap, 500 seems ok.
+             */
             template <typename MyPointT> // TODO: remove cloud normal estimation to separate function
             static int
             extractPlane( pcl::ModelCoefficients::Ptr                         &out_coefficients,
@@ -26,7 +40,7 @@ namespace smartgeom
                           pcl::PointIndices::Ptr                              *out_inlier_indices  = NULL,
                           pcl::PointIndices::ConstPtr                          in_indices          = pcl::PointIndices::ConstPtr(),
                           double                                               distanceThreshold   = .1,
-                          int                                                  sacmodel            = pcl::SACMODEL_NORMAL_PLANE,
+                          int                                                  sac_model           = pcl::SACMODEL_NORMAL_PLANE,
                           Eigen::Vector3f                                     *p_normal            = NULL,
                           int                                                  normal_estimation_K = 20,
                           int                                                  sac_method          = pcl::SAC_RANSAC,
